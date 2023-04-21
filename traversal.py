c0ac1689c783479e7a2537b23ef88fa627eb43cb
Paulo Elpidio Alves Sampaio <paulo-elpidio.alves-sampaio@MBA-10822987.local>
2023-04-21 16:41:04 +0200

    node selector

--- traversal.py
+++ traversal.py
@@ -1,11 +1,12 @@
 from sinac.core import *
 from sinac import transform
 
-def select_child(node):
+def node_selector_child(node):
     if isinstance(node, BinaryOperation):
-        print('0) Back')
+        print('0) Up')
         print('1) ' + str(node.left))
         print('2) ' + str(node.right))
+        print('3) Select')
         choice = int(input())
 
         if choice == 0:
@@ -14,14 +15,19 @@ def select_child(node):
             return node.left
         if choice == 2:
             return node.right
+        if choice == 3:
+            return node
     else:
-        print('0) Back')
+        print('0) Up')
+        print('1) Select')
         choice = int(input())
 
         if choice == 0:
             return 'parent'
+        if choice == 1:
+            return node
 
-def select_node(expr):
+def node_selector(expr):
     traversal = [expr]
 
     while len(traversal) > 0:
@@ -30,15 +36,12 @@ def select_node(expr):
         print("\n=========================")
         print(current_node)
 
-        print("1) Change node")
-        print("2) Select node") 
-        choice = int(input())
-
-        if choice == 1:
-            selected_node = select_child(current_node)
-            if selected_node == 'parent':
-                traversal.pop()
-            elif selected_node is not None:
-                traversal.append(selected_node)
-        if choice == 2:
-            return current_node
+        selected_node = node_selector_child(current_node)
+        if selected_node == 'parent':
+            traversal.pop()
+        elif selected_node == current_node:
+            return selected_node
+        elif selected_node is not None:
+            traversal.append(selected_node)
+    return None
+    
